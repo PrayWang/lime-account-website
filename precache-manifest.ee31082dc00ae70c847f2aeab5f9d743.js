47ba7d3220d3cf6118ec9286b857982c0ebbec10
PrayWang <346076741@qq.com>
2022-02-16 13:59:32 +0800

    deploy

---
+++ precache-manifest.ee31082dc00ae70c847f2aeab5f9d743.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa536665cc8f0f082cde2a307bce4a5",
+    "url": "/lime-account-website/QRCODE.png"
+  },
+  {
+    "revision": "46ec96de25ecb615b788",
+    "url": "/lime-account-website/css/app.efd4de2d.css"
+  },
+  {
+    "revision": "30a47cc6e6c824312b5dd5f11eba3619",
+    "url": "/lime-account-website/img/lime.30a47cc6.png"
+  },
+  {
+    "revision": "020dd546e577a70bab82a4b859e2b524",
+    "url": "/lime-account-website/index.html"
+  },
+  {
+    "revision": "46ec96de25ecb615b788",
+    "url": "/lime-account-website/js/app.94598c85.js"
+  },
+  {
+    "revision": "ffbcda735a1183020670",
+    "url": "/lime-account-website/js/chunk-vendors.a43a502f.js"
+  },
+  {
+    "revision": "c23492496f26f87fe7e11bb811667f73",
+    "url": "/lime-account-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/lime-account-website/robots.txt"
+  }
+]);
